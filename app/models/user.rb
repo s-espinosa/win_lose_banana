a97f48060d9cfa8a35d05f46ed685150f8c9d340
Sal Espinosa <sespinos@gmail.com>
2016-05-02 11:31:37 -0600

    Update styling based on iPhone testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
   def flavor_text
     case role
     when "winner"
-      "<p><strong>Tell them now!</strong></p><p>You are the banana judge,<br>the grand plátano assessor,<br>knower of all fruits yellow!</p><p>Let them prove to you their worth!</p>".html_safe
+      "<p><strong>Tell them now!</strong></p><p>Banana judge,<br>plátano assessor,<br>knower of all fruits yellow!</p><p>Let them prove to you their worth!</p>".html_safe
     when "loser"
-      "<p>Don't tell them that!</p><p>Lie!<br><strong>Lie, you fool!</strong></p><p>Convince them that you're the banana that you always wished you could be!</p>".html_safe
+      "<p>Don't tell them that!</p><p>Lie!<br><strong>Lie, you fool!</strong></p><p>Convince them you're the banana you always wished you could be!</p>".html_safe
     when "banana"
-      "<p>But the loser is a dirty liar!</p><p>Don't let him convince the winner with his lies!</p><p><strong>You are the one true banana.</strong></p><p>Only you can save the winner from this treachery!</p>".html_safe
+      "<p>The loser is a dirty liar!</p><p>Don't let him convince the winner with his lies!</p><p><strong>You are the one true banana.</strong></p>".html_safe
     else
       "Sorry, start a new game, or find another group of three."
     end
